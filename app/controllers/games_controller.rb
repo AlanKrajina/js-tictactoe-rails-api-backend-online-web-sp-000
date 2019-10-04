63d2a71fd9c42e012858af6281e2273f34267339
Alan Krajina <alankrajina88@gmail.com>
2019-10-04 09:42:31 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,6 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.create(game_params)
-#    test2 = @test["state"]
-#    @game = JSON.parse(test2)
     render json: @game, status: 201
   end
 
